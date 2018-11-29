192b55e8363c676231713236b453e47047346020
Joyce-YunjiaoGuo <yunjiaog@uci.edu>
2018-11-29 09:06:02 -0800

    react uppy

--- src/containers/Upload.js
+++ src/containers/Upload.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './Upload.css';
 
 const Uppy = require('@uppy/core');
@@ -9,26 +8,20 @@ const Dropbox = require('@uppy/dropbox');
 const Instagram = require('@uppy/instagram');
 const Webcam = require('@uppy/webcam');
 const AwsS3 = require('@uppy/aws-s3');
-// const DragDrop = require('@uppy/drag-drop');
-// const ProgressBar = require('@uppy/progress-bar');
-// const Tus = require('@uppy/tus');
 
-class App extends Component {
+
+class Upload extends Component {
     constructor(props) {
         super(props);
         this.state = {
             uppyPreviews: []
         };
 
-        this.uppyOne = new Uppy({ debug: true, autoProceed: false});
-        this.reader = new FileReader();
-
-        // this.uploadFile = this.uploadFile.bind(this);
-        // this.addFileToState = this.addFileToState.bind(this);
+        this.uppy = new Uppy({ debug: true, autoProceed: false });
     }
 
     componentDidMount() {
-        this.uppyOne
+        this.uppy
             .use(Dashboard, {
                 trigger: '.UppyModalOpenerBtn',
                 inline: false,
@@ -49,19 +42,19 @@ class App extends Component {
                     console.log("i am going to upload files", files);
                 }
             })
-            .use(GoogleDrive, { target: Dashboard, serverUrl: 'https://companion.uppy.io' })
-            .use(Dropbox, { target: Dashboard, serverUrl: 'https://companion.uppy.io' })
-            .use(Instagram, { target: Dashboard, serverUrl: 'https://companion.uppy.io' })
+            .use(GoogleDrive, { target: Dashboard, serverUrl: 'http://localhost:3010' })
+            .use(Dropbox, { target: Dashboard, serverUrl: 'http://localhost:3010' })
+            .use(Instagram, { target: Dashboard, serverUrl: 'http://localhost:3010' })
             .use(Webcam, { target: Dashboard })
             .use(AwsS3, {
                 limit: 0,
                 timeout: 30000,
-                serverUrl: 'https://simplenotes.netlify.com'
+                serverUrl: 'http://localhost:8080'
             })
-            
+
             .on('file-added', (file) => {
                 console.log('Added file', file)
-              })
+            })
             .on("file-removed", file => {
                 console.log("removing", file);
             })
@@ -75,22 +68,13 @@ class App extends Component {
                 console.log("failed ", result.failed);
                 console.log("successful ", result.successful);
             })
-            
 
 
-        this.uppyOne.on('complete', (result) => {
+
+        this.uppy.on('complete', (result) => {
             console.log(result);
         });
 
-        // this.uppyOne.on('file-added', (file) => {
-        //     console.log('Added file', file);
-
-        //     this.reader.onload = (readerEvt) =>
-        //         this.addFileToState({ file, base64: readerEvt.target.result });
-        //     // Define this onload every time to get file and base64 every time
-        //     this.reader.readAsDataURL(file.data);
-        // });
-        // couldn't figure a better solution on the spot
     }
 
 
@@ -99,49 +83,10 @@ class App extends Component {
             <div className="Upload">
                 <button className="UppyModalOpenerBtn">Add File</button>
                 <div className="DashboardContainer">
-                    {/* <div className="UppyDragDrop" />
-                    <div className="UppyDragDrop-Progress" /> */}
-                    {/* <button onClick={this.uploadFile}>Log previews to console / Submit</button> */}
                 </div>
             </div>
         );
     }
 }
 
-export default App;
-
-// type uppy_file_t = {
-//   data: { /* File */
-//     lastModified: float,
-//     lastModifiedDate: Js.Date.t,
-//     name: string,
-//     size: float,
-//     type: string,
-//     webkitRelativePath: string,
-//   },
-//   extension: string,
-//   id: string,
-//   isRemote: bool,
-//   meta: {
-//     name: string,
-//     type: string,
-//   },
-//   name: string,
-//   preview: undefined,
-//   progress: {
-//     bytesTotal: float,
-//     bytesUploaded: float,
-//     percentage: int,
-//     uploadComplete: bool,
-//     uploadStarted: bool,
-//   },
-//   remote: string,
-//   size: float,
-//   source: string,
-//   type: string,
-// };
-
-// type uppy_preview_t = {
-//   file: uppy_file_t,
-//   base64: string,
-// };
+export default Upload;
